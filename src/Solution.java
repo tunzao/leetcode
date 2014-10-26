8c16d3e4dc618be3c02cdfe54f9976fc251dfe77
tunzao <l.tunzao@gmail.com>
2014-10-26 22:47:23 +0800

    first missing positive int

--- src/Solution.java
+++ src/Solution.java
@@ -6,11 +6,40 @@ import java.util.Arrays;
 public class Solution {
     public static void main(String[] args) {
         Solution s = new Solution();
-        System.out.print(s.reverse(-123));
+        System.out.print(s.firstMissingPositive(new int[]{1, 1}));
+    }
+
+    /**
+     * https://oj.leetcode.com/problems/first-missing-positive/
+     * 好吧，这道题是我抄别人的
+     * 解决问题的关键是第一个消失的正整数在1-n+1之间
+     *
+     * @param A
+     * @return
+     */
+    public int firstMissingPositive(int A[]) {
+
+        if (A.length == 0) {
+            return 1;
+        }
+        int[] e = new int[A.length + 2];
+        for (int a : A) {
+            if (a <= A.length + 1 && a > 0) {
+                e[a] = 1;
+            }
+        }
+
+        for (int i = 1; i < A.length + 2; i++) {
+            if (e[i] == 0) {
+                return i;
+            }
+        }
+        return 1;
     }
 
     /**
      * not solved yet
+     *
      * @param numbers
      * @param target
      * @return
@@ -46,6 +75,7 @@ public class Solution {
     /**
      * https://oj.leetcode.com/problems/count-and-say/
      * so easy
+     *
      * @param n
      * @return
      */
@@ -77,20 +107,67 @@ public class Solution {
     /**
      * https://oj.leetcode.com/problems/reverse-integer/
      * so easy
+     *
      * @param x
      * @return
      */
     public int reverse(int x) {
         int y = 0;
-        int flag = x<0 ? -1 : 1;
+        int flag = x < 0 ? -1 : 1;
         x = Math.abs(x);
         while (x > 0) {
             int b = x % 10;
-            y = y*10 + b;
+            y = y * 10 + b;
             x /= 10;
         }
 
-        return y*flag;
+        return y * flag;
+    }
+
+    public int aoti(String str) {
+        str = str.toLowerCase().trim();
+        if (str.length() == 0) {
+            return 0;
+        }
+        int x = 0;
+        int flag = 1;
+        int base = 10;
+        int start = 0;
+        if (str.charAt(0) == '+') {
+            start = 1;
+        } else if (str.charAt(0) == '-') {
+            start = 1;
+            flag = -1;
+        }
+        if (str.charAt(start) == '0') {
+            if (str.charAt(start + 1) == 'x') {
+                start += 2;
+                base = 16;
+            } else {
+                start += 1;
+                base = 8;
+            }
+        }
+        if (base != 16) {
+            for (; start < str.length(); start++) {
+                x = x * base + str.charAt(start) - '0';
+            }
+        } else {
+            x = atoi16(str, start);
+        }
+        return x * flag;
+    }
+
+    private int atoi16(String str, int start) {
+        int x = 0;
+        for (; start < str.length(); start++) {
+            if (str.indexOf(start) > '9') {
+                x = x * 16 + str.charAt(start) - 'a' + 10;
+            } else {
+                x = x * 16 + str.charAt(start) - '0';
+            }
+        }
+        return x;
     }
 
 }
