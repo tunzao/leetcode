0badec89ee3488baa46433611a14e961d5699441
tunzao <l.tunzao@gmail.com>
2021-09-05 17:56:02 +0800

    add ReadWithRead4

---
+++ src/ReadWithRead4.java
@@ -0,0 +1,44 @@
+
+/**
+ * @author likuan
+ */
+public class ReadWithRead4 extends Reader4 {
+
+    /**
+     * @param buf Destination buffer
+     * @param n   Number of characters to read
+     * @return The number of actual characters read
+     */
+    public int read(char[] buf, int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        char[] four = new char[4];
+        int read = 0;
+        while (read < n) {
+            int buffRead = read4(four);
+            if (read + buffRead > n) {
+                append(buf, read, four, n - read);
+                return n;
+            } else if (buffRead > 0){
+                append(buf, read, four, buffRead);
+            }
+            read += buffRead;
+            if (buffRead < 4) {
+                return read;
+            }
+        }
+        return n;
+    }
+
+    private void append(char[] root, int offset, char[] append, int len) {
+        if (len >= 0) System.arraycopy(append, 0, root, offset, len);
+
+    }
+}
+
+class Reader4 {
+    public int read4(char[] buf) {
+        return 0;
+    }
+}
