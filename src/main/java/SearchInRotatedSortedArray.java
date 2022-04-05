4775e2c346f30c1e9f71e01bd7c87919a0c77d05
tunzao <l.tunzao@gmail.com>
2022-04-05 17:56:10 +0800

    add SearchInRotatedSortedArray

---
+++ src/main/java/SearchInRotatedSortedArray.java
@@ -0,0 +1,82 @@
+import java.util.function.ObjIntConsumer;
+
+/**
+ * 整数数组 nums 按升序排列，数组中的值互不相同 。
+ * <p>
+ * 在传递给函数之前，nums 在预先未知的某个下标 k（0 <= k < nums.length）上进行了 旋转，使数组变为 [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]]（下标 从 0 开始 计数）。例如， [0,1,2,4,5,6,7] 在下标 3 处经旋转后可能变为 [4,5,6,7,0,1,2] 。
+ * <p>
+ * 给你 旋转后 的数组 nums 和一个整数 target ，如果 nums 中存在这个目标值 target ，则返回它的下标，否则返回 -1 。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 0
+ * 输出：4
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：nums = [4,5,6,7,0,1,2], target = 3
+ * 输出：-1
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：nums = [1], target = 0
+ * 输出：-1
+ * <p>
+ * 提示：
+ * <p>
+ * 1 <= nums.length <= 5000
+ * -10^4 <= nums[i] <= 10^4
+ * nums 中的每个值都 独一无二
+ * 题目数据保证 nums 在预先未知的某个下标上进行了旋转
+ * -10^4 <= target <= 10^4
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/search-in-rotated-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * @author likuan
+ */
+public class SearchInRotatedSortedArray {
+
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        if (n == 0) {
+            return -1;
+        }
+        if (n == 1) {
+            return nums[0] == target ? 0 : -1;
+        }
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int mid = (l + r) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[0] <= nums[mid]) {
+                if (nums[0] <= target && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[n - 1]) {
+                    l = mid + 1;
+                } else {
+                    r = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        SearchInRotatedSortedArray searchInRotatedSortedArray = new SearchInRotatedSortedArray();
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{1}, 0));
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{1}, 1));
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{1, 3}, 3));
+//        System.out.println(searchInRotatedSortedArray.search(new int[]{1, 3}, 0));
+        System.out.println(searchInRotatedSortedArray.search(new int[]{4,5,1,2,3}, 1));
+    }
+}
